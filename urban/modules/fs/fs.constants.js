388e1109e68d6a6d918a7c788134459ae036e6fc
Nikoloz Pestvenidze <pestvenidzenika@gmail.com>
2022-10-13 18:43:46 +0400

    create fs module

---
+++ urban/modules/fs/fs.constants.js
@@ -0,0 +1,7 @@
+const FsConstants = {
+  TEMP_DIRECTORY: process.env.TEMP_DIRECTORY,
+  UPLOAD_DIRECTORY: process.env.UPLOAD_DIRECTORY,
+  EXTRACT_DIRECTORY: process.env.EXTRACT_DIRECTORY,
+};
+
+export default FsConstants;
